ecffeb52fe20cff787914b68d894b1d219f6d6b0
alissonbk <alissonbk2010@hotmail.com>
2025-02-13 16:31:20 -0300

    fix: script tsinit

--- scripts/tsinit.sh
+++ scripts/tsinit.sh
@@ -5,6 +5,6 @@ npx npm install -D tsx typescript @types/node;
 npx tsc --init
 touch index.ts;
 echo "console.log(\"hello world\")" > index.ts;
-sed -i '/"scripts": {/a \\t \ \"start\": \"node index.ts\",' package.json;
+sed -i '/"scripts": {/a \\t \ \"start\": \"tsx index.ts\",' package.json;
 
 echo 'done, `npm run start` to run your project';

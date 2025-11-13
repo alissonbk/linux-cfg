ca1419b745f1793ee772d8963193096645f49a46
alissonbk <alissonbk2010@hotmail.com>
2025-11-13 09:16:53 -0300

    update set-display

---
+++ scripts/set-display-home.sh
@@ -0,0 +1,2 @@
+xrandr --output HDMI-1-0 --primary --mode 1920x1080 --brightness 1 --right-of eDP-1 -r 165
+xrandr --output HDMI-1-0 --primary --mode 1920x1080 --brightness 1 --right-of eDP-1 -r 165
--- scripts/set-display.sh
+++ scripts/set-display.sh
@@ -1,1 +1,2 @@
-xrandr --output HDMI-1-0 --primary --mode 2560x1080 --right-of eDP-1
+xrandr --output HDMI-1-0 --primary --mode 2560x1080 --brightness 1 --right-of eDP-1 -r 120
+xrandr --output HDMI-1-0 --primary --mode 2560x1080 --brightness 1 --right-of eDP-2 -r 120

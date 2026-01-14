3894df140fbc86d5acb69dc7021696611a905b9b
alissonbk <alissonbk2010@hotmail.com>
2026-01-13 22:15:28 -0300

    nix: add scripts to vm

--- nix/vm.nix
+++ nix/vm.nix
@@ -37,22 +37,22 @@ in
   networking.wireless.enable = true; # wpa_supplicant
 
   # VM config
-  virtualisation.vmVariant = {
-    virtualisation.resolution = {
-      x = 1280;
-      y = 1024;
-    };
-    virtualisation.qemu.options = [
-      # Better display option
-      "-vga virtio"
-      "-display gtk,zoom-to-fit=false,show-cursor=on"
-      # Enable copy/paste
-      # https://www.kraxel.org/blog/2021/05/qemu-cut-paste/
-      "-chardev qemu-vdagent,id=ch1,name=vdagent,clipboard=on"
-      "-device virtio-serial-pci"
-      "-device virtserialport,chardev=ch1,id=ch1,name=com.redhat.spice.0"
-    ];
-  };
+  # virtualisation.vmVariant = {
+  #   virtualisation.resolution = {
+  #     x = 1280;
+  #     y = 1024;
+  #   };
+  #   virtualisation.qemu.options = [
+  #     # Better display option
+  #     "-vga virtio"
+  #     "-display gtk,zoom-to-fit=false,show-cursor=on"
+  #     # Enable copy/paste
+  #     # https://www.kraxel.org/blog/2021/05/qemu-cut-paste/
+  #     "-chardev qemu-vdagent,id=ch1,name=vdagent,clipboard=on"
+  #     "-device virtio-serial-pci"
+  #     "-device virtserialport,chardev=ch1,id=ch1,name=com.redhat.spice.0"
+  #   ];
+  # };
 
   users.groups.${username} = { };
 
